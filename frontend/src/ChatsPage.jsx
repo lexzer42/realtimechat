85efb65aec26793caa9b595d2f25e4178104b793
lexzer42 <134535937+lexzer42@users.noreply.github.com>
2023-06-01 14:52:26 -0400

    ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
  return (
   <div style={{ height: '100vh' }}>
     <PrettyChatWindow
-          projectId='d09d852c-1511-4bc5-a963-b86c55859cf8'
+          projectId='agregar-project-id'
           username={props.user.username}
           secret={props.user.secret}
           style={{ height: '100%'}}
